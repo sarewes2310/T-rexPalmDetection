5e7a896df29b5bf672ebefabacfdd9d4bb77967d
Sarewes <35771197+sarewes2310@users.noreply.github.com>
2021-04-19 13:52:04 +0700

    Modifikasi main.py

--- main.py
+++ main.py
@@ -2,32 +2,21 @@ import cv2
 import numpy as np
 import mediapipe as mp
 import keyboard
-import time
-
-def genXLandmark(HLandmark,
-                 hands,
-                 length):
-  return round(HLandmark.landmark[hands].x * length)
-
-def genYLandmark(HLandmark,
-                 hands,
-                 length):
-  return round(HLandmark.landmark[hands].y * length)
 
 def main():
-  mp_drawing = mp.solutions.drawing_utils
-  mp_hands = mp.solutions.hands
-  cap = cv2.VideoCapture(1) # Mengambil data dari video
-  bts_bawah = 40
-  jarak_minimal = 50
+  mp_drawing = mp.solutions.drawing_utils # Variabel untuk menambahkan landmark ke dalam telapak tangan
+  mp_hands = mp.solutions.hands # Variabel untuk memanggil class untuk deteksi telapak tangan
+  cap = cv2.VideoCapture(0) # Variabel untuk mengambil gambar dari webcam
+  bts_bawah = 40 # Variabel nilai batas bawah
+  jarak_minimal = 50 # Variabel nilai minimal dalam bentuk persen untuk trigger kontrol permainan
 
   with mp_hands.Hands(max_num_hands=1,
-                      min_detection_confidence=0.5,
+                      min_detection_confidence=0.7,
                       min_tracking_confidence=0.7) as hands:
     while cap.isOpened():
+      # Mengambil gambar dari webcam
       success, image = cap.read()
-      image_hight, image_width, _ = image.shape
-
+      image_height, image_width, _ = image.shape
       if not success:
         print('[+] Camera kosong!!!')
         continue
@@ -36,15 +25,17 @@ def main():
       # Merubah warna dari BGR ke RGB
       image = cv2.cvtColor(cv2.flip(image, 1),
                            cv2.COLOR_BGR2RGB)
-      # To improve performance, optionally mark the image as not writeable to
-      # pass by reference.
+
+      # Buat image menjadi tidak bisa dimodifikasi agar mempercepat proses perhitungan
       image.flags.writeable = False
       results = hands.process(image)
-
-      # Draw the hand annotations on the image.
       image.flags.writeable = True
+
+      # Rubah format gambar dari RGB ke BGR
       image = cv2.cvtColor(image,
                            cv2.COLOR_RGB2BGR)
+
+      # Cek apakah tangan ditemukan pada webcam
       if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
           # Mengambil nilai x dari jari telunjuk
@@ -54,7 +45,7 @@ def main():
           # Mengambil nilai y dari jari telunjuk
           jarakYIFT = genYLandmark(HLandmark=hand_landmarks,
                                    hands=mp_hands.HandLandmark.INDEX_FINGER_TIP,
-                                   length=image_hight)
+                                   length=image_height)
 
           # Mengambil nilai x dari ibu jari
           jarakXTT = genXLandmark(HLandmark=hand_landmarks,
@@ -63,7 +54,7 @@ def main():
           # Mengambil nilai y dari ibu jari
           jarakYTT = genYLandmark(HLandmark=hand_landmarks,
                                   hands=mp_hands.HandLandmark.THUMB_TIP,
-                                  length=image_hight)
+                                  length=image_height)
 
           # Menambahkan landmark ke dalam gambar
           mp_drawing.draw_landmarks(image,
@@ -98,6 +89,16 @@ def main():
       if cv2.waitKey(5) & 0xFF == 27:
         break
   cap.release()
+
+def genXLandmark(HLandmark,
+                 hands,
+                 length):
+  return round(HLandmark.landmark[hands].x * length)
+
+def genYLandmark(HLandmark,
+                 hands,
+                 length):
+  return round(HLandmark.landmark[hands].y * length)
 
 if __name__ == '__main__':
   main()
